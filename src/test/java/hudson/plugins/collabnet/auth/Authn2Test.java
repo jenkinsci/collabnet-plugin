5d4eb4c67f447f0ec8ec1e661b0d9f7c3f3ebb4b
kohsuke <kohsuke@dev.java.net>
2010-05-11 17:44:28 +0000

    test is made more comprehensive

--- src/test/java/hudson/plugins/collabnet/auth/Authn2Test.java
+++ src/test/java/hudson/plugins/collabnet/auth/Authn2Test.java
@@ -1,6 +1,12 @@
 package hudson.plugins.collabnet.auth;
 
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import hudson.model.Describable;
+import hudson.model.Descriptor;
+import hudson.security.SecurityRealm;
 import org.jvnet.hudson.test.HudsonTestCase;
+
+import java.util.concurrent.Callable;
 
 /**
  * Test CollabNet Authentication.
@@ -12,9 +18,43 @@ public class Authn2Test extends HudsonTestCase {
      * Verifies that the UI is bound correctly to properties
      */
     public void testConfigRoundtrip() throws Exception {
-        CollabNetSecurityRealm original = new CollabNetSecurityRealm("http://www.google.com/", true, true);
+        roundtripAndAssert(new CollabNetSecurityRealm("http://www.google.com/", true, true));
+        roundtripAndAssert(new CollabNetSecurityRealm("http://www.collab.net/", false, false));
+    }
+
+    private void roundtripAndAssert(CollabNetSecurityRealm original) throws Exception {
         hudson.setSecurityRealm(original);
         submit(createWebClient().goTo("configure").getFormByName("config"));
         assertEqualBeans(original, hudson.getSecurityRealm(), "collabNetUrl,enableSSOAuthFromCTF,enableSSOAuthToCTF");
+    }
+
+    /**
+     * Makes sure that help link exists on all three options.
+     */
+    public void testHelpLink() throws Exception {
+        assertHelpExists(CollabNetSecurityRealm.class,"collabNetUrl,enableSSOAuthFromCTF,enableSSOAuthToCTF");
+    }
+
+    /**
+     * Asserts that help files exist for the specified properties of the given instance.
+     *
+     * @param type
+     *      The describable class type that should have the associated help files.
+     * @param properties
+     *      ','-separated list of properties whose help files should exist.
+     */
+    public void assertHelpExists(final Class<? extends Describable> type, final String properties) throws Exception {
+        executeOnServer(new Callable<Object>() {
+            public Object call() throws Exception {
+                Descriptor d = hudson.getDescriptor(type);
+                WebClient wc = createWebClient();
+                for (String property : properties.split(",")) {
+                    String url = d.getHelpFile(property);
+                    assertNotNull("Help file for the property "+property+" is missing on "+type, url);
+                    wc.goTo(url); // make sure it successfully loads
+                }
+                return null;
+            }
+        });
     }
 }
