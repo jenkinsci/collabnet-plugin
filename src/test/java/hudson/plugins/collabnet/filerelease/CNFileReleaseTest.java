b64ed3ef2687a8a1d3d48dd494a75dd8995290f6
kohsuke <kohsuke@dev.java.net>
2010-06-03 01:03:07 +0000

    [artf65728] Merged the test case.

--- src/test/java/hudson/plugins/collabnet/filerelease/CNFileReleaseTest.java
+++ src/test/java/hudson/plugins/collabnet/filerelease/CNFileReleaseTest.java
@@ -1,14 +1,27 @@
 package hudson.plugins.collabnet.filerelease;
 
+import com.collabnet.ce.webservices.CollabNetApp;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.model.FreeStyleProject;
 import hudson.plugins.collabnet.CNHudsonTestCase;
+import hudson.plugins.collabnet.ConnectionFactory;
 import hudson.plugins.collabnet.documentuploader.FilePattern;
+import hudson.plugins.collabnet.util.CNHudsonUtil;
+import org.jvnet.hudson.test.TestBuilder;
+
+import java.io.IOException;
 
 /**
  * Test document upload facility.
- *
- * Unlike {@link FileReleaseTest}, this version doesn't have any setUp/tearDown.
  */
 public class CNFileReleaseTest extends CNHudsonTestCase {
+    private static final String PACKAGE = System.getProperty("fr_package");
+    private static final String RELEASE = System.getProperty("fr_release");
+    private static final String FILE = "test.txt";
+    private static final String FILE_CONTENT = "Test file from FileRelease";
+
     public void testConfigRoundtrip() throws Exception {
         setGlobalConnectionFactory();
 
@@ -27,6 +40,41 @@ public class CNFileReleaseTest extends CNHudsonTestCase {
      */
     public void testHelpLink() throws Exception {
         assertHelpExists(CNFileRelease.class,FIELDS+",-connectionFactory");
+    }
+
+    /**
+     * Test FileRelease upload by building and uploading a test file.
+     */
+    public void testUpload() throws Exception {
+        if(!isOnline()) return; // skip if offline
+
+        FreeStyleProject job = this.createFreeStyleProject();
+        job.getPublishersList().add(new CNFileRelease(
+                new ConnectionFactory(CN_URL,TEST_USER,TEST_PW),
+                CN_PROJECT_NAME, PACKAGE, RELEASE, true,
+                new FilePattern[]{new FilePattern(FILE)}
+        ));
+        job.getBuildersList().add(new TestBuilder() {
+            @Override
+            public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+                build.getWorkspace().child(FILE).write(FILE_CONTENT,"UTF-8");
+                return true;
+            }
+        });
+        buildAndAssertSuccess(job);
+        this.verifyFRUpload();
+    }
+
+    /**
+     * Verify that an upload of the test file was successful.
+     */
+    public void verifyFRUpload() {
+        CollabNetApp cna = CNHudsonUtil.getCollabNetApp(CN_URL, TEST_USER,
+                                                        TEST_PW);
+        assert(cna != null);
+        String fileId = CNHudsonUtil.getFileId(cna, CN_PROJECT_NAME, PACKAGE,
+                                               RELEASE, FILE);
+        assert(fileId != null);
     }
 
     private static final String FIELDS = "connectionFactory,project,pkg,release,filePatterns,overwrite";
